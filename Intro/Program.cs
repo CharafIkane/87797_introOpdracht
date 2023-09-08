acd91187942925ffdf639521314a557633ea5822
CharafIkane <98458476+CharafIkane@users.noreply.github.com>
2023-09-08 15:45:53 +0200

    ss

---
+++ Intro/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Student
+{
+    string id;
+    string volledigeNaam;
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        Console.WriteLine("Welkom op de C# pagina van Charaf");
+        Console.WriteLine("Wat is jouw naam?");
+        s1.volledigeNaam = Console.ReadLine();
+        Console.Write("Hallo " + s1.volledigeNaam + ", ");
+        Console.WriteLine("Wat is uw studentnummer?");
+        s1.id = Console.ReadLine();
+        Console.WriteLine("Herhaal uw studentnummer");
+        string herhaal = Console.ReadLine();
+        if (s1.id.Equals(herhaal))
+        {
+            Console.WriteLine("Uw studentnummer is correct!");
+        }
+        else
+        {
+            Console.WriteLine("Uw studentnummer is verkeerd ingevoerd");
+        }
+        Console.ReadLine();
+    }
+}
+
