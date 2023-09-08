4020711551651ada21394ceb622706b8d1cdaf5e
CharafIkane <98458476+CharafIkane@users.noreply.github.com>
2023-09-08 15:30:21 +0200

    Derde stap

---
+++ script.js
@@ -0,0 +1,26 @@
+let naam = window.prompt("Wat is uw naam?");
+var foto = document.getElementById("welkom_foto");
+
+if (naam == "") {
+    document.getElementById("welkom").innerHTML = "Welkom!";
+} else if (naam > -10000) {
+    document.getElementById("welkom").innerHTML = "Welkom!";
+} else {
+    document.getElementById("welkom").innerHTML = "Welkom " + naam + "!";
+}
+
+const image = ["media/charaf.jpg", "media/pipboy.png", "img3"];
+
+function imageChange() {
+
+    foto.src = image[0];
+}
+
+imageChange();
+
+foto.addEventListener("click", function () {
+    var foto = document.getElementById("welkom_foto");
+    foto.src = image[1];
+    console.log("klik")
+});
+
